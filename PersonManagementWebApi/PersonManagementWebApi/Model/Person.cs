4bd797a4767fd23f483c17a2f775aa4d4e93e2af
AzhMans <91619992+AzhMans@users.noreply.github.com>
2024-01-12 20:20:34 +0400

    Add files via upload

---
+++ PersonManagementWebApi/PersonManagementWebApi/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonManagementWebApi.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
